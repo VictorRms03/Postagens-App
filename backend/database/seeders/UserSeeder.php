99338c2b6f52a202b2f7628935e07a93c6838cee
Victor Ramos <victorrms03@gmail.com>
2025-08-20 10:49:22 -0300

    fix[backend]: updateOrCreate on UserSeeder

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 
@@ -13,7 +14,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::updateOnCreate(
+        User::updateOrCreate(
             ['email' => 'teste@gmail.com'],
             [
                 'name' => 'Victor Ramos',
