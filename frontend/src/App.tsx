4c24e2d6536e079fa537e0e40192de3f4628399e
Victor Ramos <victorrms03@gmail.com>
2025-08-21 14:30:28 -0300

    fix[frontend]: App.tsx now works

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,23 +6,8 @@ function App() {
   
   const [loggedIn, setLoggedIn] = useState(!!localStorage.getItem('token')); // State to verify if user is logged
 
-  return (
-
-    <html lang="pt_BR" className="h-full bg-white">
-
-      <head>
-        <meta charSet="UTF-8" />
-        <title>Postagens App</title>
-      </head>
-
-      <body className="h-full">
-        {loggedIn ? <Posts/> : <Login onLoggedIn={() => setLoggedIn(true)} />}
-      </body>
-
-    </html>
-
-  );                                    
-
+  return loggedIn ? <Posts/> : <Login onLoggedIn={() => setLoggedIn(true)} />
+  
 }
 
 export default App
--- frontend/src/Posts.tsx
+++ frontend/src/Posts.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
 import api from './api';
-import './App.css'
 
 function Posts() {
 
