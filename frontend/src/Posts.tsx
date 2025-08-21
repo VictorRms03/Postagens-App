0ae6d0d7f347642e1bcbec488a851c4da3f35400
Victor Ramos <victorrms03@gmail.com>
2025-08-21 01:30:07 -0300

    feat[frontend]: list posts

--- frontend/src/Posts.tsx
+++ frontend/src/Posts.tsx
@@ -1,14 +1,45 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import api from './api';
 import './App.css'
 
 function Posts() {
 
+  type Post = { 
+    id: number; 
+    title: string; 
+    content: string; 
+    user: { 
+      id: number; 
+      name: string 
+    };
+  };
+
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  async function getPosts() {
+    
+    const { data } = await api.get('/posts');
+
+    setPosts(data);
+    
+  }
+
+  useEffect(() => { getPosts(); }, []);
+
   return (
-    <>
-      TELA DE POSTS
-    </>
-  )
+    <div>
+      <h2>Postagens</h2>
+      <ul>
+        {posts.map(p => (
+          <li key={p.id} >
+            <h3>{p.title}</h3> <span> — usuário: {p.user?.name ?? 'Desconhecido'}</span>
+            <p>{p.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Posts
